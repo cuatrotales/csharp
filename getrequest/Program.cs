47af4408527161f1ca44059b822ba9054710906f
cuatrotales <jfquinterogu14@gmail.com>
2022-10-13 14:17:20 -0500

    Cambia la cantidad de informacion

--- getrequest/Program.cs
+++ getrequest/Program.cs
@@ -5,7 +5,7 @@ internal class Program
 {
     private static void Main(string[] args)
     {
-        string API_URL = "https://jsonplaceholder.typicode.com/todos?_limit=5";
+        string API_URL = "https://jsonplaceholder.typicode.com/todos?_limit=10";
         var client = new WebClient();
         var json = client.DownloadString(API_URL);
         dynamic posts = JsonConvert.DeserializeObject(json);

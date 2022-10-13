a0328ceea99e30b82c30e1b0befc3aafce6435f2
cuatrotales <jfquinterogu14@gmail.com>
2022-10-13 14:15:01 -0500

    aplicacion de consola c#

---
+++ getrequest/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Net;
+using Newtonsoft.Json;
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        string API_URL = "https://jsonplaceholder.typicode.com/todos?_limit=5";
+        var client = new WebClient();
+        var json = client.DownloadString(API_URL);
+        dynamic posts = JsonConvert.DeserializeObject(json);
+
+        foreach (var post in posts)
+        {
+            Console.WriteLine(post.id + ": "+ post.title);
+        }
+    }
+}
